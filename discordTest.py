31417c12a6b3cb31a8a5e3f2bfd47cd3567a80d2
Mcdonoughd <dmcdonough@wpi.edu>
2018-01-12 20:01:33 -0500

    PULLING

--- discordTest.py
+++ discordTest.py
@@ -6,8 +6,9 @@ from discord.ext.commands import Bot
 import asyncio
 
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='!')
 
+client = discord.Client()
 
 
 
@@ -23,6 +24,23 @@ async def ping(ctx):
     print ("user has pinged")
 
 @bot.command(pass_context=True)
+async def hello(ctx):
+    await bot.say("HELLO WORLD!")
+    print("Hello World!")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('$greet'):
+        await client.send_message(message.channel, 'Say hello')
+        msg = await client.wait_for_message(author=message.author, content='hello')
+        await client.send_message(message.channel, 'Hello.')
+
+@bot.command(pass_context=True)
+async def myid(ctx):
+    await bot.say("YOUR ID IS: " + ctx.author.id)
+    print("Your ID is: " + ctx.author)
+
+@bot.command(pass_context=True)
 async def info(ctx, user: discord.Member):
     await bot.say("The users name is: {}".format(user.name))
     await bot.say("The users ID is: {}".format(user.id))
@@ -34,4 +52,7 @@ async def info(ctx, user: discord.Member):
 async def kick(ctx, user: discord.Member):
     await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
     await bot.kick(user)
-bot.run('MzkyOTE3OTU1NDQ2MzA4ODY1.DRuPOw.Z3aGgdvDuKP8wAkHMt2vSPSEwZ4')
+
+bot.run('NDAxNTM4OTU0NDg4MTg0ODMy.DTrriQ.y2QzATd4j8PVsHkuhlwv7Azmnyc')
+#SURYA AUTH: MzkyOTE3OTU1NDQ2MzA4ODY1.DRuPOw.Z3aGgdvDuKP8wAkHMt2vSPSEwZ4
+#DAN AUTH: NDAxNTM4OTU0NDg4MTg0ODMy.DTrriQ.y2QzATd4j8PVsHkuhlwv7Azmnyc
