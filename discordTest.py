c04aedbed6794a63a03eb9392aa628f6fbba4846
Surya Vadivazhagu <vsuryav@gmail.com>
2018-01-13 00:09:40 -0500

    Can take input + stores files as a CSV file within the directory.

--- discordTest.py
+++ discordTest.py
@@ -4,10 +4,10 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-
+import csv
+import time
 
 bot = commands.Bot(command_prefix='!')
-
 client = discord.Client()
 
 
@@ -16,6 +16,11 @@ async def on_ready():
     print ('Ready when you are xd')
     print ("I am running on " + bot.user.name)
     print ("With the ID: " + bot.user.id)
+@client.event
+async def on_ready():
+    print ('Ready when you are xd')
+    print ("I am running on " + client.user.name)
+    print ("With the ID: " + client.user.id)
 
 @bot.command(pass_context=True)
 async def ping(ctx):
@@ -27,13 +32,16 @@ async def hello(ctx):
     await bot.say("HELLO WORLD!")
     print("Hello World!")
 
-@bot.event
+@client.event
 async def on_message(message):
-    if message.content.startswith("!greet"):
-        await bot.send_message(message.channel, "Say hello")
-        msg = await bot.wait_for_message(author=message.author, content="hello")
-        await bot.send_message(message.channel, "Hello.")
+    if message.content.startswith('$start'):
+        await client.send_message(message.channel, 'Type $stop 4 times.')
+        for i in range(4):
+            msg = await client.wait_for_message(author=message.author, content='$stop')
+            fmt = '{} left to go.' + message.author.id
+            await client.send_message(message.channel, fmt.format(3 - i))
 
+        await client.send_message(message.channel, 'Good job!')
 @bot.command(pass_context=True)
 async def myid(ctx):
     await bot.say("YOUR ID IS: " + ctx.author.id)
@@ -54,11 +62,19 @@ async def kick(ctx, user: discord.Member):
 
 @bot.command(pass_context=True)
 async def add(ctx, arg):
-    quotes = '"'
-    word = arg + quotes
+    word = arg
     await bot.say(word)
-#client.run('NDAxNTM4OTU0NDg4MTg0ODMy.DTrriQ.y2QzATd4j8PVsHkuhlwv7Azmnyc')
-bot.run('NDAxNTM4OTU0NDg4MTg0ODMy.DTrriQ.y2QzATd4j8PVsHkuhlwv7Azmnyc')
+
+@bot.command(pass_context=True)
+async def addGame(ctx, message):
+    splitMsg = ctx.message.content.split(" ")
+    with open("gameList.csv", "a") as csvfile:
+        filewriter =csv.writer(csvfile, delimiter=',',
+                               quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow([splitMsg[1]])
+    await bot.say(splitMsg[1] + " has been added to the game list.")
+#client.run('MzkyOTE3OTU1NDQ2MzA4ODY1.DRuPOw.Z3aGgdvDuKP8wAkHMt2vSPSEwZ4')
+bot.run('MzkyOTE3OTU1NDQ2MzA4ODY1.DRuPOw.Z3aGgdvDuKP8wAkHMt2vSPSEwZ4')
 
 #SURYA AUTH: MzkyOTE3OTU1NDQ2MzA4ODY1.DRuPOw.Z3aGgdvDuKP8wAkHMt2vSPSEwZ4
 #DAN AUTH: NDAxNTM4OTU0NDg4MTg0ODMy.DTrriQ.y2QzATd4j8PVsHkuhlwv7Azmnyc
